e5e31e4ba419eefa03e60d4dc2d698b04bfec901
robotian <robotian@gmail.com>
2026-08-27 19:09:49 +0000

    outdoor ready

--- mtu32_bringup/launch/bringup_main.launch.py
+++ mtu32_bringup/launch/bringup_main.launch.py
@@ -166,16 +166,16 @@ def launch_setup(context, *args, **kwargs):
                 condition=IfCondition(use_mocap_fake_localizer),
             ),  
 
-            # Node(
-            #     package='depthimage_to_laserscan',
-            #     executable='depthimage_to_laserscan_node',
-            #     name='depthimage_to_laserscan',
-            #     namespace=f'/{namespace}',
-            #     remappings=remappings_tf +[('depth', f'/{namespace}/sensors/camera_0/depth/image'),
-            #                 ('depth_camera_info', f'/{namespace}/sensors/camera_0/depth/camera_info'),
-            #                 ('scan', f'/{namespace}/sensors/camera_0/scan')],
-            #     parameters=[depth2scan_param_config]
-            # ),  
+            Node(
+                package='depthimage_to_laserscan',
+                executable='depthimage_to_laserscan_node',
+                name='depthimage_to_laserscan',
+                namespace=f'/{namespace}',
+                remappings=remappings_tf +[('depth', f'/{namespace}/sensors/camera_0/depth/image'),
+                            ('depth_camera_info', f'/{namespace}/sensors/camera_0/depth/camera_info'),
+                            ('scan', f'/{namespace}/sensors/camera_0/scan')],
+                parameters=[depth2scan_param_config]
+            ),  
 
             Node(
                 package='apriltag_ros',
--- mtu32_bringup/launch/bringup_nav2_mapless_j100.launch.py
+++ mtu32_bringup/launch/bringup_nav2_mapless_j100.launch.py
@@ -135,7 +135,8 @@ def launch_setup(context, *args, **kwargs):
     # see if we've overridden the scan_topic
     eval_scan_topic = scan_topic.perform(context)
     if len(eval_scan_topic) == 0:
-        eval_scan_topic = f'/{namespace}/sensors/lidar2d_0/scan_filtered'  # both robots are using lidar sensors for navigation, so default to that if no override is provided
+        eval_scan_topic = f'/{namespace}/sensors/camera_0/scan' 
+        # eval_scan_topic = f'/{namespace}/sensors/lidar2d_0/scan_filtered'  # both robots are using lidar sensors for navigation, so default to that if no override is provided
     
     params_file = PathJoinSubstitution([get_package_share_directory('mtu32_bringup'), 'config', f'{platform_model}', 'nav2_mapless.yaml'])
 
